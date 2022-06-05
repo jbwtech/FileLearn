c5915d808d55eaf798856ef04012f9cc88bb71e8
Brent Williams <jbwtech@gmail.com>
2022-06-05 12:20:49 -0400

    Initial Commit

---
+++ FileLearn/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  FileLearn
+//
+//  Created by jbwtech on 6/5/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
